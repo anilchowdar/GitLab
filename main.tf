1398dd256e12315dbbd3722d0669a2cf9c0db309
Anil Kumar <kumarchowdary467@gmail.com>
2021-03-15 20:53:32 -0400

    fourth commit

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance
 " "example" {
     ami = "var.ami_id"
-    instance_type = "var.instabnce_type"
+    instance_type = "varrrrrrrr.instabnce_type"
     region = "var.regionnnn"
     
 }
