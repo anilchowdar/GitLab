9c37f37024adcef1f6415e3652d494f909338a16
Anil Kumar <kumarchowdary467@gmail.com>
2021-03-15 20:55:05 -0400

    seciongf

--- main.tf
+++ main.tf
@@ -1,7 +1,11 @@
 resource "aws_instance
 " "example" {
     ami = "var.ami_id"
+<<<<<<< HEAD
     instance_type = "varrrrrrrr.instabnce_type"
     region = "var.regionnnn"
+=======
+    instance_type = "var.instabnce_type"
+>>>>>>> parent of 5738a5d (third commit)
     
 }
---
+++ vim mainn.tf
@@ -0,0 +1,2 @@
+vim mainn.tf
+
